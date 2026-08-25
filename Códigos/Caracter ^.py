1b97e571954377dc27af0b729cfc7c225b8e2b37
Miguel Angel Plazas Llanes <est.miguel.plazas@unimilitar.edu.co>
2026-08-24 20:39:58 -0500

    Add files via upload

---
+++ Códigos/Caracter ^.py
@@ -0,0 +1,9 @@
+from machine import Pin , UART
+import utime
+
+uart = UART (0, baudrate =9600 , bits =8, parity =None , stop =1,
+tx=Pin (0) , rx=Pin (1) )
+
+while True :
+uart . write ("^")
+utime . sleep (1)
